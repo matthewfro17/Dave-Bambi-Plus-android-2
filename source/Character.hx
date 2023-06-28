913d784ebeae5e35cc489988e82e4b4a296e0826
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-06-28 18:26:37 -0400

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -843,7 +843,25 @@ class Character extends FlxSprite
 				antialiasing = false;
 		
 				playAnim('idle');
+			case 'homer':
+				frames = Paths.getSparrowAtlas('homer/Homer', 'shared');
+				
+				animation.addByPrefix('idle', 'Idle', 24, false);
+				for (anim in ['left', 'down', 'up', 'right'])
+				{
+					animation.addByPrefix('sing${anim.toUpperCase()}', anim, 24, false);
+				}
+				loadOffsetFile(curCharacter);
+				
+				globalOffset = [0, 0];
+				
+				barColor = FlxColor.fromString("0x619BC1");
 
+				setGraphicSize(Std.int((width * 0.8) / furiosityScale));
+				updateHitbox();
+				antialiasing = false;
+		
+				playAnim('idle');
 			case 'bambi-new':
 				frames = Paths.getSparrowAtlas('bambi/bambiRemake', 'shared');
 				animation.addByPrefix('idle', 'bambi idle', 24, false);
